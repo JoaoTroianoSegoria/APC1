557a5be25f4447e16ccd77ddb56ec6489adfb315
JoaoTroianoSegoria <160737815+JoaoTroianoSegoria@users.noreply.github.com>
2024-03-06 13:52:16 +0000

    Códifo da aula 2 e da prática 1

---
+++ AULAS1/variaveis.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int numero;
+  char caracter;
+  int a; // essa variav
+  int x;
+  float nime_composto;
+  
+  numero = 10;
+
+  numero = 100
+
+  return 0;
+}
